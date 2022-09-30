2678ceeb45a631e32d60cd634b8fc235843d37cc
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2022-09-30 19:49:58 +0100

    added new element min max temp visbility

--- weather-app-react-api/src/App.js
+++ weather-app-react-api/src/App.js
@@ -36,11 +36,20 @@ const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=
           <div className="temp">
           {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}     {/*data.main had to be used fist because it did not like the dat.main.temp as it would not recognise the temp child. &toFixed() remove decimals */}
           </div>
+          <div className="mintemp">
+          {data.main ? <p>{data.main.temp_min.toFixed()}°C Min Temp</p> : null}
+          
+          </div>
+          <div className="maxtemp">
+          {data.main ? <p>{data.main.temp_max.toFixed()}°C Max Temp</p> : null}
+          
+          </div>
+
           <div className="description">
-          {data.weather ? <p>{data.weather[0].main}</p> : null}   {/* so weatehr was in an object so had to follow it similrly to temp but link to the array */}
+          {data.weather ? <p>{data.weather[0].main}</p> : null}   {/* so weather was in an object so had to follow it similar to temp but link to the array */}
           </div>
         </div> 
-{/*data.name undefined beans the bottom div will no show unless you types osmetig in the the enter locatin input box */}
+{/*data.name undefined beans the bottom div will no show unless you type in the the enter location input box */}
 {data.name !== undefined &&   
       
        <div className="bottom">
@@ -53,9 +62,13 @@ const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=
         <p>Humidity</p>
         </div>  
         <div className="wind">
-        {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> :null}
+        {data.wind ? <p className="bold">{data.wind.speed.toFixed()} MPH</p> : null}
          <p>Wind Speed</p>
         </div>  
+        <div className="visibility">
+        {data.main ? <p className="bold">{data.visibility.toFixed()/100} %</p> : null}
+        <p>Visibility</p>
+        </div>
        </div>
       }  
 
